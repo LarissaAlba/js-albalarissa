e56616837690e166999105b5d6d440a763ad1014
Larissa Alba <alba.santos.larissa@escola.pr.gov.br>
2024-08-14 13:59:03 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(600, 600);//cria um retangulo de x de largura por y de altura 
+  background("pink");//acrecenta uma cor de fundo} fiil("green");
+
+}
+
+function draw() {
+
+fill("orange");
+
+if(mouseIsPressed){
+  rect(mouseX,mouseY,40,60); 
+  
+}
+
+}
+  
+  
+  
+    
